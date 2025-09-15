a07a9186b79e2323a05f3aaa6db4537a365fd1dd
Charlie Davis Cordova Arias <charlie.cordova34@gmail.com>
2025-09-14 23:08:17 -0500

    Subiendo el backend inicial

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Backend funcionando en Render 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
